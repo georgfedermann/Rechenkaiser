7cc0111fda3068d75aca4fda0b6738854340fea9
Georg Federmann <georg.federmann@gmail.com>
2019-01-01 02:09:39 +0100

    Implement level management

--- Rechenkaiser/Controller/ViewController.swift
+++ Rechenkaiser/Controller/ViewController.swift
@@ -14,11 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageLeft: UIImageView!
     @IBOutlet weak var diceImageRight: UIImageView!
     @IBOutlet weak var answerTextfield: UITextField!
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var levelIcon: UIImageView!
     
     // MARK: data required by the business logic
     var summand1:Int = 0;
     var summand2:Int = 0;
     var sum:Int = 0;
+    
+    // MARK: level management
+    // to proceed to next level, 15 challenges have to be done correctly
+    // on the first try in sequence
+    var correctStreakCount:Int = 0;
+    // how many correct answers in a row are necessary for a level pump
+    var correctStreakSizeForLevelPump:Int = 15;
+    var currentLevel = 1;
     
     let motivationLibrary:MotivationLibrary = MotivationLibrary();
     
@@ -33,17 +43,31 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated);
         answerTextfield.becomeFirstResponder();
     }
-
+    
     // MARK: event handlers
     @IBAction func confirmButtonPressed(_ sender: UIButton) {
         if let playerAnswerString = answerTextfield.text,
             let playerAnswer = Int(playerAnswerString),
             playerAnswer == sum {
             ProgressHUD.showSuccess(motivationLibrary.getRandomQuoteForCorrectAnswer());
+            correctStreakCount += 1;
+            checkLevelClimb();
             createChallenge();
             updateView();
         } else {
             ProgressHUD.showError(motivationLibrary.getRandomQuoteForIncorrectAnswer());
+            ProgressHUD.showError("You lose a level 😖");
+            correctStreakCount = 0;
+            currentLevel = max(1, currentLevel - 1);
+            updateView();
+        }
+    }
+    
+    func checkLevelClimb(){
+        if(correctStreakCount == correctStreakSizeForLevelPump){
+            ProgressHUD.showSuccess("You gain a level 🏅");
+            correctStreakCount = 0;
+            currentLevel = min(2, currentLevel + 1);
         }
     }
     
@@ -51,8 +75,13 @@ class ViewController: UIViewController {
     // displayed as dice faces, the sum has to be entered by the player in
     // the text field
     func createChallenge(){
-        summand1 = Int.random(in:1...6);
-        summand2 = Int.random(in:1...6);
+        if(currentLevel == 1){
+            summand1 = Int.random(in:1...6);
+            summand2 = Int.random(in:1...6);
+        } else if (currentLevel == 2){
+            summand1 = Int.random(in:1...9);
+            summand2 = Int.random(in:1...9);
+        }
         sum = summand1 + summand2;
     }
     
@@ -62,6 +91,8 @@ class ViewController: UIViewController {
         diceImageLeft.image = UIImage(named:"dice\(summand1)");
         diceImageRight.image = UIImage(named:"dice\(summand2)");
         answerTextfield.text = "";
+        levelLabel.text = "Level \(currentLevel)";
+        levelIcon.image = UIImage(named:"lvl\(currentLevel)");
     }
     
 }
